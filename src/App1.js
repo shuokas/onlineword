f71b3366a0c0801917ecc7aacd704f3ccbe5edec
yangshuo <yangshuo@teamsun.com.cn>
2023-01-05 15:23:39 +0800

    init project

---
+++ src/App1.js
@@ -0,0 +1,31 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App1() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <code>
+          class name
+        </code>
+        创建最新
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        测试提高
+
+      </header>
+    </div>
+  );
+}
+
+export default App1;
